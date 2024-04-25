6f09051abf0494a84ad7762f1e66c55318b6b519
alexandre <alexandrealvescs@gmail.com>
2024-04-25 14:39:24 -0300

    feat: HomePage implemented  in route management

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from '../pages/login';
+import HomePage from '../pages/home';
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        {/* rotas aqui */}
+        <Route path="/home" element={<HomePage />}/>
       </Routes>
     </Router>
   );
