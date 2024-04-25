420c38f45d7df63fc9267161521471a0e300238a
alexandre <alexandrealvescs@gmail.com>
2024-04-25 12:28:09 -0300

    feat: using LoginRequisition in onSubmit function and added error message

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import {
@@ -8,29 +8,30 @@ import {
   Button,
   ErrorMessage,
 } from './styled';
+import { LoginRequisition } from '../../requests';
 
 const LoginPage: React.FC = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const navigate = useNavigate();
+    const [error, setError] = useState<string>('');
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const navigate = useNavigate();
 
-  const onSubmit = (data: any) => {
-    console.log('#####################');
-    console.log(data);
-    console.log('#####################');
-    navigate('/home');
-  };
+    const onSubmit = (data: any) => {
+        LoginRequisition(data, setError)
+        navigate('/home');
+    };
 
-  return (
-    <LoginContainer>
-      <LoginForm onSubmit={handleSubmit(onSubmit)}>
-        <Input {...register('username', { required: true })} placeholder="Username" />
-        {errors.username && <ErrorMessage>This field is required</ErrorMessage>}
-        <Input {...register('password', { required: true })} type="password" placeholder="Password" />
-        {errors.password && <ErrorMessage>This field is required</ErrorMessage>}
-        <Button type="submit">Login</Button>
-      </LoginForm>
-    </LoginContainer>
-  );
+    return (
+        <LoginContainer>
+        <LoginForm onSubmit={handleSubmit(onSubmit)}>
+            <Input {...register('email', { required: true })} placeholder="Email" />
+            {errors.username && <ErrorMessage>This field is required</ErrorMessage>}
+            <Input {...register('password', { required: true })} type="password" placeholder="Password" />
+            {errors.password && <ErrorMessage>This field is required</ErrorMessage>}
+            <Button type="submit">Login</Button>
+            {error && <ErrorMessage>{error}</ErrorMessage>}
+        </LoginForm>
+        </LoginContainer>
+    );
 };
 
 export default LoginPage;
